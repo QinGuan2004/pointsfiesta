fa3f675263a41342d883d5218b119a3e65b7815b
Sebastian Choo <sebastian_choo_yong_qiang@s2017.sst.edu.sg>
2018-11-04 04:16:31 +0800

    New UI/UX: Sign In Overhauled

--- SST House Points App/SignInViewController.swift
+++ SST House Points App/SignInViewController.swift
@@ -12,13 +12,7 @@ import FirebaseAuth
 
 class SignInViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
 
-
-    @IBOutlet weak var Points: UILabel!
     @IBOutlet var googleSignInButton: GIDSignInButton!
-
-    @IBAction func didTapSignOut(_ sender: AnyObject) {
-        GIDSignIn.sharedInstance()?.signOut()
-    }
 
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
         if let error = error {
@@ -57,31 +51,11 @@ class SignInViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDele
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        Points.text = "Points\n This app hopes to increase the House Culture in SST. \nSign in to Google with your SST Google Account."
-// Setting Background Colour (Gradient)
-        let layer = CAGradientLayer()
-        layer.frame = view.bounds
-        view.layer.addSublayer(layer)
-        layer.colors = [UIColor(red: 0.08, green: 0.08, blue: 0.08, alpha: 1.0).cgColor, UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.0).cgColor, UIColor(red: 0.08, green: 0.08, blue: 0.08, alpha: 1.0).cgColor]
-        layer.startPoint = CGPoint(x: 0, y: 0)
-        layer.endPoint = CGPoint(x: 1, y: 1)
-        layer.opacity = 0.85
-        layer.zPosition = -1
-
-        let layer1 = CAGradientLayer()
-        layer1.frame = view.bounds
-        view.layer.addSublayer(layer1)
-        layer1.colors = [UIColor(red: 0.08, green: 0.08, blue: 0.08, alpha: 1.0).cgColor, UIColor(red: 0.60, green: 0.60, blue: 0.60, alpha: 1.0).cgColor, UIColor(red: 0.08, green: 0.08, blue: 0.08, alpha: 1.0).cgColor]
-        layer1.startPoint = CGPoint(x: 1, y: 0)
-        layer1.endPoint = CGPoint(x: 0, y: 1)
-        layer1.opacity = 0.85
-        layer1.zPosition = -1
-        googleSignInButton.style = .wide
-        googleSignInButton.colorScheme = .dark
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
         // Do any additional setup after loading the view.
         GIDSignIn.sharedInstance().clientID = "803179619735-g2pqb4rvenqpj9vrqsj98hqib19ioh0m.apps.googleusercontent.com"
         GIDSignIn.sharedInstance().delegate = self
-        GIDSignIn.sharedInstance().uiDelegate = self
     }
 
     // ...
