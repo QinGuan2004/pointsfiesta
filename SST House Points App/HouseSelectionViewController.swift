1b9b54d30f88ad45da19973fcf0086b788301b4d
Sebastian Choo <sebastian_choo_yong_qiang@s2017.sst.edu.sg>
2018-11-08 01:46:23 +0800

    More UI Changes (House Selection)

--- SST House Points App/HouseSelectionViewController.swift
+++ SST House Points App/HouseSelectionViewController.swift
@@ -15,11 +15,15 @@ import Firebase
 class HouseSelectionViewController: UIViewController {
 
 
-    @IBOutlet weak var blackButton: UIButton!
-    @IBOutlet weak var redButton: UIButton!
-    @IBOutlet weak var blueButton: UIButton!
-    @IBOutlet weak var greenButton: UIButton!
-    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var blackView: UIView!
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    
+    @IBOutlet weak var continueLabel: UIButton!
+    
+    var houseSelect: String = "No House"
     
     var ref: DatabaseReference! = Database.database().reference()
     var ref1: DocumentReference? = nil
@@ -30,68 +34,84 @@ class HouseSelectionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Config Buttons
-        blackButton.layer.cornerRadius = 10
-        redButton.layer.cornerRadius = 10
-        blueButton.layer.cornerRadius = 10
-        greenButton.layer.cornerRadius = 10
-        yellowButton.layer.cornerRadius = 10
-        
-        // Firebase FireStore
-        settings.areTimestampsInSnapshotsEnabled = true
-        db.settings = settings
-        
+        continueLabel.layer.cornerRadius = 25
+        blackView.layer.cornerRadius = 30
+        redView.layer.cornerRadius = 30
+        blueView.layer.cornerRadius = 30
+        greenView.layer.cornerRadius = 30
+        yellowView.layer.cornerRadius = 30
     }
     
-    @IBAction func blackPressed(_ sender: Any) {
+    @IBAction func blackTap(_ sender: Any) {
         UserDefaults.standard.setColor(color: UIColor(red: 0.01, green: 0.01, blue: 0.01, alpha: 1.0), forKey: "foregroundColour")
         UserDefaults.standard.setColor(color: UIColor(red: 0.41, green: 0.79, blue: 0.62, alpha: 1.0), forKey: "backgroundColour")
         UserDefaults.standard.setColor(color: UIColor(red: 0.16, green: 0.16, blue: 0.16, alpha: 1.0), forKey: "secondaryColour")
-        UserDefaults.standard.set("Black House", forKey: "houseMember")
-        ref1 = db.collection("users").addDocument(data:
-            ["house": "Black House"]) { err in
-            if let err = err {
-                print("Error adding document: \(err)")
-            } else {
-                print("Document added with ID: \(self.ref1!.documentID)")
-            }
-        }
-        performSegue(withIdentifier: "colourSelected", sender: sender)
+        houseSelect = "Black House"
+        blackView.layer.borderColor = UIColor.purple.cgColor
+        blackView.layer.borderWidth = 10
+        redView.layer.borderWidth = 0
+        blueView.layer.borderWidth = 0
+        greenView.layer.borderWidth = 0
+        yellowView.layer.borderWidth = 0
     }
-
-    @IBAction func redPressed(_ sender: Any) {
+    
+    @IBAction func redTap(_ sender: Any) {
         UserDefaults.standard.setColor(color: UIColor(red: 0.93, green: 0.13, blue: 0.05, alpha: 1.0), forKey: "foregroundColour")
         UserDefaults.standard.setColor(color: UIColor(red: 0.35, green: 0.47, blue: 0.57, alpha: 1.0), forKey: "backgroundColour")
         UserDefaults.standard.setColor(color: UIColor(red: 0.62, green: 0.25, blue: 0.22, alpha: 1.0), forKey: "secondaryColour")
-        UserDefaults.standard.set("Red House", forKey: "houseMember")
-        performSegue(withIdentifier: "colourSelected", sender: sender)
-    }
-
-    @IBAction func bluePressed(_ sender: Any) {
-        UserDefaults.standard.setColor(color: UIColor(red: 0.00, green: 0.64, blue: 1.00, alpha: 1.0), forKey: "foregroundColour")
-        UserDefaults.standard.setColor(color: UIColor(red: 0.09, green: 0.04, blue: 0.11, alpha: 1.0), forKey: "backgroundColour")
-        UserDefaults.standard.setColor(color: UIColor(red: 0.00, green: 0.46, blue: 0.73, alpha: 1.0), forKey: "secondaryColour")
-        UserDefaults.standard.set("Blue House", forKey: "houseMember")
-        performSegue(withIdentifier: "colourSelected", sender: sender)
-    }
-
-    @IBAction func greenPressed(_ sender: Any) {
-        UserDefaults.standard.setColor(color: UIColor(red: 0.00, green: 0.60, blue: 0.00, alpha: 1.0), forKey: "foregroundColour")
-        UserDefaults.standard.setColor(color: UIColor(red: 0.93, green: 0.38, blue: 0.33, alpha: 1.0), forKey: "backgroundColour")
-        UserDefaults.standard.setColor(color: UIColor(red: 0.00, green: 0.44, blue: 0.00, alpha: 1.0), forKey: "secondaryColour")
-        UserDefaults.standard.set("Green House", forKey: "houseMember")
-        performSegue(withIdentifier: "colourSelected", sender: sender)
-    }
-
-    @IBAction func yellowPressed(_ sender: Any) {
-        UserDefaults.standard.setColor(color: UIColor(red: 0.98, green: 0.89, blue: 0.20, alpha: 1.0), forKey: "foregroundColour")
-        UserDefaults.standard.setColor(color: UIColor(red: 0.93, green: 0.31, blue: 0.13, alpha: 1.0), forKey: "backgroundColour")
-        UserDefaults.standard.setColor(color: UIColor(red: 0.71, green: 0.55, blue: 0.23, alpha: 1.0), forKey: "secondaryColour")
-        UserDefaults.standard.set("Yellow House", forKey: "houseMember")
-        performSegue(withIdentifier: "colourSelected", sender: sender)
+        houseSelect = "Red House"
+        redView.layer.borderColor  = UIColor.purple.cgColor
+        redView.layer.borderWidth = 10
+        blueView.layer.borderWidth = 0
+        greenView.layer.borderWidth = 0
+        yellowView.layer.borderWidth = 0
     }
 
     
+    @IBAction func blueTap(_ sender: Any) {
+        UserDefaults.standard.setColor(color: UIColor(red: 0.00, green: 0.64, blue: 1.00, alpha: 1.0), forKey: "foregroundColour")
+        UserDefaults.standard.setColor(color: UIColor(red: 0.09, green: 0.04, blue: 0.11, alpha: 1.0), forKey: "backgroundColour")
+        UserDefaults.standard.setColor(color: UIColor(red: 0.00, green: 0.46, blue: 0.73, alpha: 1.0), forKey: "secondaryColour")
+        houseSelect = "Blue House"
+        blueView.layer.borderWidth = 10
+        blueView.layer.borderColor = UIColor.purple.cgColor
+        redView.layer.borderWidth = 0
+        greenView.layer.borderWidth = 0
+        yellowView.layer.borderWidth = 0
+    }
 
+    @IBAction func greenTap(_ sender: Any) {
+        UserDefaults.standard.setColor(color: UIColor(red: 0.00, green: 0.60, blue: 0.00, alpha: 1.0), forKey: "foregroundColour")
+        UserDefaults.standard.setColor(color: UIColor(red: 0.93, green: 0.38, blue: 0.33, alpha: 1.0), forKey: "backgroundColour")
+        UserDefaults.standard.setColor(color: UIColor(red: 0.00, green: 0.44, blue: 0.00, alpha: 1.0), forKey: "secondaryColour")
+        houseSelect = "Green House"
+        greenView.layer.borderWidth = 10
+        greenView.layer.borderColor = UIColor.purple.cgColor
+        redView.layer.borderWidth = 0
+        blueView.layer.borderWidth = 0
+        yellowView.layer.borderWidth = 0
+    }
+
+    
+    @IBAction func yellowTap(_ sender: Any) {
+        UserDefaults.standard.setColor(color: UIColor(red: 0.98, green: 0.89, blue: 0.20, alpha: 1.0), forKey: "foregroundColour")
+        UserDefaults.standard.setColor(color: UIColor(red: 0.93, green: 0.31, blue: 0.13, alpha: 1.0), forKey: "backgroundColour")
+        UserDefaults.standard.setColor(color: UIColor(red: 0.71, green: 0.55, blue: 0.23, alpha: 1.0), forKey: "secondaryColour")
+            houseSelect = "Yellow House"
+        yellowView.layer.borderWidth = 10
+        yellowView.layer.borderColor = UIColor.purple.cgColor
+        redView.layer.borderWidth = 0
+        blueView.layer.borderWidth = 0
+        greenView.layer.borderWidth = 0
+        yellowView.layer.borderWidth = 0
+    }
+
+    
+    @IBAction func continueButton(_ sender: Any) {
+        UserDefaults.standard.set("\(houseSelect)", forKey: "houseMember")
+        performSegue(withIdentifier: "houseSelected", sender: self)
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -103,7 +123,6 @@ class HouseSelectionViewController: UIViewController {
     */
 
 }
-
 extension UserDefaults {
     func colorForKey(key: String) -> UIColor? {
         var color: UIColor?
